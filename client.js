91dcbbf7591956de586b157a6ef481733bc37dd4
John McLear <john@mclear.co.uk>
2020-11-22 17:44:17 +0000

    autofixes from Etherpad checkPlugins.js

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
-var eejs = require("ep_etherpad-lite/node/eejs");
+const eejs = require('ep_etherpad-lite/node/eejs');
 
 exports.eejsBlock_indexWrapper = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_search/templates/search.html", {}, module);
+  args.content += eejs.require('ep_search/templates/search.html', {}, module);
   return cb();
 };
--- index.js
+++ index.js
@@ -1,37 +1,29 @@
-// Main job is to check pads periodically for activity and notify owners when someone begins editing and when someone finishes. 
-var db = require('ep_etherpad-lite/node/db/DB').db;
-var async = require('ep_etherpad-lite/node_modules/async');
+// Main job is to check pads periodically for activity and notify owners when someone begins editing and when someone finishes.
+const db = require('ep_etherpad-lite/node/db/DB').db;
+const async = require('ep_etherpad-lite/node_modules/async');
 
 // Settings -- EDIT THESE IN settings.json not here..
 // var pluginSettings = settings.ep_search;
 // var checkFrequency = pluginSettings.checkFrequency || 60000; // 10 seconds
 
 exports.registerRoute = function (hook_name, args, cb) {
+  args.app.get('/search', (req, res) => {
+    const searchString = req.query.query;
+    const result = {};
 
-  args.app.get('/search', function(req, res) {
-
-    var searchString = req.query["query"];
-    var result = {};
-
-    db.findKeys("pad:*", "*:*:*", function(err, pads){ // get all pads
-
-      async.forEachSeries(pads, function(pad, callback){
-
-        db.get(pad, function(err, padData){ // get the pad contents
-          var padText = padData.atext.text || "";
+    db.findKeys('pad:*', '*:*:*', (err, pads) => { // get all pads
+      async.forEachSeries(pads, (pad, callback) => {
+        db.get(pad, (err, padData) => { // get the pad contents
+          const padText = padData.atext.text || '';
           // does searchString exist in aText?
           if (padText.toLowerCase().indexOf(searchString.toLowerCase()) !== -1) {
             result.pad = pad;
           }
           callback();
         });
-       
-      }, function(err){
+      }, (err) => {
         res.send(JSON.stringify(result));
       });
-
     });
-
   });
-
 };
