f856cd67e8ac4b5556efc2da37e73b859e47c10a
John McLear <john@mclear.co.uk>
2021-02-04 15:33:41 +0000

    lint

--- client.js
+++ client.js
@@ -1,6 +1,8 @@
+'use strict';
+
 const eejs = require('ep_etherpad-lite/node/eejs');
 
-exports.eejsBlock_indexWrapper = function (hook_name, args, cb) {
+exports.eejsBlock_indexWrapper = (hookName, args, cb) => {
   args.content += eejs.require('ep_search/templates/search.html', {}, module);
   return cb();
 };
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-// Main job is to check pads periodically for activity and notify owners when someone begins editing and when someone finishes.
+'use strict';
+
+// Main job is to check pads periodically for activity and notify owners
+// when someone begins editing and when someone finishes.
 const db = require('ep_etherpad-lite/node/db/DB').db;
 const async = require('ep_etherpad-lite/node_modules/async');
 
@@ -6,7 +9,7 @@ const async = require('ep_etherpad-lite/node_modules/async');
 // var pluginSettings = settings.ep_search;
 // var checkFrequency = pluginSettings.checkFrequency || 60000; // 10 seconds
 
-exports.registerRoute = function (hook_name, args, cb) {
+exports.registerRoute = (hookName, args, cb) => {
   args.app.get('/search', (req, res) => {
     const searchString = req.query.query;
     const result = {};
